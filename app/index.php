93ea66ca75cc38d5cb2a3e92399cd6e82ba74316
Elliot Pettingale <elipettingale@gmail.com>
2017-09-13 09:53:41 +0100

    Modify index.php to work when run on windows

--- app/index.php
+++ app/index.php
@@ -10,5 +10,11 @@ $generation_rules = array (
     5 => "Buzz",
 );
 
-$input = readline("Please enter a value between $min_range and $max_range: ");
+if (PHP_OS == 'WINNT') {
+    echo "Please enter a value between $min_range and $max_range: ";
+    $input = stream_get_line(STDIN, 1024, PHP_EOL);
+} else {
+    $input = readline("Please enter a value between $min_range and $max_range: ");
+}
+
 echo generate_fizz_buzz($input, $min_range, $max_range, $generation_rules);
