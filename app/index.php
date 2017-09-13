abb6ea8bcb95a91607577d85a9a78ed035222419
Elliot Pettingale <elipettingale@gmail.com>
2017-09-13 09:16:51 +0100

    Change input validation variable names to match function for clarity

--- app/index.php
+++ app/index.php
@@ -2,13 +2,13 @@
 
 require 'functions.php';
 
-$min_input = 1;
-$max_input = 2000;
+$min_range = 1;
+$max_range = 2000;
 
 $generation_rules = array (
     3 => "Fizz",
     5 => "Buzz",
 );
 
-$input = readline("Please enter a value between $min_input and $max_input: ");
-echo generate_fizz_buzz($input, $min_input, $max_input, $generation_rules);
+$input = readline("Please enter a value between $min_range and $max_range: ");
+echo generate_fizz_buzz($input, $min_range, $max_range, $generation_rules);
