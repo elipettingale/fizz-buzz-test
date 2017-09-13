4bea54eb7a03962929c269e10b3c71cdf9d5bf6a
Elliot Pettingale <elipettingale@gmail.com>
2017-09-13 08:27:11 +0100

    Add validation to testing

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -12,4 +12,15 @@ final class AppTest extends TestCase
         );
     }
 
+    public function test_basic_validation() {
+        $this->assertEquals(
+            generate_fizz_buzz(0),
+            "Invalid Input: Input must be an integer between 1 and 2000"
+        );
+        $this->assertEquals(
+            generate_fizz_buzz(2001),
+            "Invalid Input: Input must be an integer between 1 and 2000"
+        );
+    }
+
 }
