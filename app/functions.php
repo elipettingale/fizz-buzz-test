b7a140e3c53328d0dd317606066ef77cffe920fb
Elliot Pettingale <elipettingale@gmail.com>
2017-09-13 08:25:17 +0100

    Build generate_fizz_buzz function

--- app/functions.php
+++ app/functions.php
@@ -1,5 +1,29 @@
 <?php
 
 function generate_fizz_buzz(int $input) {
-    return "";
+
+    $sequence = "";
+
+    for ($i=1; $i<=$input; ++$i) {
+
+        $step = "";
+
+        if ($i % 3 == 0) {
+            $step .= "Fizz";
+        }
+
+        if ($i % 5 == 0) {
+            $step .= "Buzz";
+        }
+
+        if ($step == "") {
+            $step = $i;
+        }
+
+        $sequence .= $step . ", ";
+
+    }
+
+    return $sequence;
+
 }
