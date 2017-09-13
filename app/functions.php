3c2fe9dd1ed860d92d2247828a9857298517e5fc
Elliot Pettingale <elipettingale@gmail.com>
2017-09-13 08:32:10 +0100

    Add input validation to main function

--- app/functions.php
+++ app/functions.php
@@ -2,6 +2,20 @@
 
 function generate_fizz_buzz(int $input) {
 
+    $validation = array(
+        'options' => array(
+            'min_range' => 1,
+            'max_range' => 2000,
+        )
+    );
+
+    if (filter_var($input, FILTER_VALIDATE_INT, $validation) != True) {
+        return "Invalid Input: Input must be an integer between "
+            . $validation['options']['min_range']
+            .  " and "
+            . $validation['options']['max_range'];
+    }
+
     $sequence = "";
 
     for ($i=1; $i<=$input; ++$i) {
